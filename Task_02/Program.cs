0253937676bfe4c884ed7c1c03167da556b4e00c
VBuser <timur.lihgt@rambler.ru>
2024-07-10 21:19:14 +0900

    Добавил решение Task_2

---
+++ Task_02/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySize, 100, 999);
+    PrintArray(array);
+    CoutnPolNumbers(array);
+}
+
+void CoutnPolNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    System.Console.WriteLine(count);
+}
+
+int[] GenerateArray(int size, int minValue, int maxValue)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempArray.Length; i++)
+        tempArray[i] = rand.Next(minValue, maxValue + 1); // (0, 10)
+
+    return tempArray;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine()); // 13
+}
+
+Main();
+
+
